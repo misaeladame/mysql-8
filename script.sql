7976d075a52018d056edc9d205344b01b060c7bc
Misael Adame <misael_adame@protonmail.com>
2022-02-16 18:13:27 -0600

    Crear una table

--- script.sql
+++ script.sql
@@ -1,1 +1,15 @@
 CREATE SCHEMA ejemplo;
+
+-- ALT + 96, comillas invertidas
+CREATE TABLE `ejemplo`.`contactos`
+(
+	`id` INT NOT NULL AUTO_INCREMENT,
+    `nombre` VARCHAR(64) NOT NULL,
+    `apellidos` VARCHAR(64) NOT NULL,
+    `direccion` VARCHAR(255) NOT NULL,
+    `celular` VARCHAR(10) NOT NULL,
+	`correo` VARCHAR(255) NOT NULL,
+    `fecha_nacimiento` DATE DEFAULT NULL,
+    `familia` TINYINT DEFAULT NULL,
+    PRIMARY KEY(`id`)
+)
