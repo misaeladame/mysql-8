b6986289778f983eec313896b18dff0e7a3cdb33
Misael Adame <misael_adame@protonmail.com>
2022-02-16 23:31:03 -0600

    Indices

--- script.sql
+++ script.sql
@@ -25,4 +25,29 @@ WHERE `id` = 2;
 DELETE FROM `ejemplo`.`contactos`
 WHERE `id` = 3;
 
+ALTER TABLE `ejemplo`.`contactos`
+ADD INDEX (nombre);
+
+ALTER TABLE `ejemplo`.`contactos`
+ADD INDEX (apellidos);
+
+ALTER TABLE `ejemplo`.`contactos`
+ADD INDEX (nombre(20));
+-- Cantidad de caracteres
+
+CREATE TABLE `ejemplo`.`contactos`
+(
+	`id` INT NOT NULL AUTO_INCREMENT,
+    `nombre` VARCHAR(64) NOT NULL,
+    `apellidos` VARCHAR(64) NOT NULL,
+    `direccion` VARCHAR(255) NOT NULL,
+    `celular` VARCHAR(10) NOT NULL,
+	`correo` VARCHAR(255) NOT NULL,
+    `fecha_nacimiento` DATE DEFAULT NULL,
+    `familia` TINYINT DEFAULT NULL,
+    PRIMARY KEY(`id`),
+    INDEX(nombre),
+    INDEX(apellidos)
+);
+
 SELECT * FROM ejemplo.contactos;
